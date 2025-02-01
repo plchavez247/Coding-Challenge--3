64ec2d52ccc01513c183dfa6cebabc797b4437d1
Pablo Chavez <plchavez@usf.edu>
2025-02-01 18:06:32 -0500

    Task 2 - Modifying Customer Orders

--- cc_3.js
+++ cc_3.js
@@ -3,3 +3,18 @@ let prices = [4,7,3,8,1];
 prices.push(6);
 prices.shift();
 console.log(prices);
+
+// Task 2: Modifying customer orders
+
+// Declaring an array with numerical quantities
+let orders = [21,14,17,22,38]
+
+// Increase the third order quantity by 5
+orders[2] +=4;
+
+//Calculate total of orders
+let total = orders.reduce((sum,price) => sum + price, 0);
+
+// Log updated array and total order
+console.log("Updated orders:", orders);
+console.log("Total order count:", total);
